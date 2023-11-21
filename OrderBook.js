e3eb7c577577855ea1b787407453f2697719c6b6
sly04 <sylvain_mahe@hotmail.fr>
2023-11-21 19:41:05 +0100

    delete JS files

--- OrderBook.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-//interface that represent an OrderBook, the contract must compute the average BTC Price of an OrderBook
-Object.defineProperty(exports, "__esModule", { value: true });
